9137402937dcd19c5c143466abc4a00d60578799
vend <8vend8@gmail.com>
2023-03-12 17:52:10 +0900

    added test hero

---
+++ Assets/Code/Game/HeroRotation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace Code.Game
+{
+    public class HeroRotation : MonoBehaviour
+    {
+        [SerializeField] private Transform _body;
+        
+        [SerializeField] private Transform _target;
+
+        private void LateUpdate()
+        {
+            Rotation();
+        }
+
+        public void ChangeTarget(Transform target) => 
+            _target = target;
+
+        private void Rotation()
+        {
+            if(_target == null)
+                return;
+
+            _body.rotation = Quaternion.LookRotation(transform.forward, _target.transform.position);
+        }
+    }
+}
